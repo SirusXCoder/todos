1ee804bba6cce328e5c1543c08bd0eef291b21ef
Joseph Coleman <sirusxcoder@gmail.com>
2024-08-08 18:38:13 -0700

    2nd commit

---
+++ cmd/todo/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"github.comm/sirusxcoder/todo-app"
+)
+
+const (
+	todoFile = ".todos.json"
+)
+
+func main() {
+	add := flag.Bool(name:"add", value: false, usage:"add a new todo")
+}
+
+flag.Parse()
+
+todos  &todo.Todos{}
+
+if err != todo.
---
+++ todo.go
@@ -0,0 +1,89 @@
+package todo
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
+)
+
+type item struct {
+	Task string
+	Done bool
+	CreatedAt time.Time
+	CompletedAt time.Time
+}
+
+type Todos []item
+
+
+func (t *Todos) Add(task string) {
+
+	todo := item{
+		Task: task,
+		Done: false,
+		CreatedAt: time.Now(),
+		CompletedAt:  time.Time{},
+	}
+	*t = append(*t,  todo)
+}
+
+
+func (t *Todos) Complete(index int) error {
+	ls := *t
+	if index <= 0 || index > len(ls) {
+		return errors.New(text:"invalid index")
+	}
+
+	ls[index-1].CompletedAt = time.Now()
+	ls[index-1].Done = true
+
+	return nil
+}
+
+
+
+func (t *Todos) Delete(index int) error {
+	ls := *t
+	if index <= 0 || index > len(ls) {
+		return errors.New(text:"invalid index")
+	}
+
+	*t  = append(ls[:index-1], ls[index:]...)
+	return nil
+}
+
+
+
+func (t *Todos) Load(filenae string) error {
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist)
+			return nil
+		}
+		return err
+
+	if len(file) == 0 {
+		return err
+	}
+	err = json.Unmarshal(file, t)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+
+func (t *Todos) Store(filename string) error {
+
+	data, err  := json.Marshal(t)
+	if err !=  nil {
+		 return err 
+	}
+	return ioutil.WriteFile(filenae, data, perm:0644 )
+}
+
+
+
