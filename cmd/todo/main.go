f1df25758779b138d28da306ca07b07c2c806545
Joseph Coleman <sirusxcoder@gmail.com>
2024-08-08 19:32:42 -0700

    fix all errors

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/sirusxcoder/todo-app/todo"
+	"github.com/sirusxcoder/todo-app"
 )
 
 const (
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/sirusxcoder/todo-app
 
 go 1.22.6
-
-require golang.org/x/tools v0.24.0
